ea63c6297bee6e894c020ad710920f61ce24350c
NtwaliEliel <n.eliel1@alustudent.com>
2025-04-16 20:58:16 +0200

    Setting Up MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config(); // Load environment variables
+const mongoose = require('mongoose');
+
 
 app.use(express.json());
 
@@ -12,3 +15,11 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
